f1d055aa997a9d96b8d5b16f449d740399941e42
Imalka Prasadini <imalkaprasadini123@gmail.com>
2022-04-15 11:43:04 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-const a =0;
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+
+app.listen(3000,()=>{
+    console.log('server is running....');
+})
