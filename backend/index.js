007984847376db54c6f8da79e0c10528e890973a
Imalka Prasadini <imalkaprasadini123@gmail.com>
2022-04-15 12:57:41 +0530

    index.js updated-DB connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql2');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
+
+
+// DB connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'simpledb',
+    port: 3306
+});
+
+// check DB connection
+
+db.connect(err=>{
+    if(err){
+        console.log('error');
+    }
+    console.log('DB connected....');
+})
+
 
 app.listen(3000,()=>{
     console.log('server is running....');
